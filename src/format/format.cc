c143ea117772f5e22b25c9d7905dfec811f45906
tanpero <3185750139@qq.com>
2019-11-30 08:59:07 +0800

    minor updae

--- src/format/format.cc
+++ src/format/format.cc
@@ -2,9 +2,9 @@
 
 static inline std::regex formattingTokens()
 {
-	std::string source = R"((\[[^\[]*\])|(\\)?([Hh]mm(ss)?|Mo|MM?M?M?|Do|DDDo|DD?D?D?|ddd?d?|do?|))"
-		+ R"(w[o|w]?|W[o|W]?|Qo?|YYYYYY|YYYYY|YYYY|YY|gg(ggg?)?|GG(GGG?)?)"
-		+ R"(|e|E|a|A|hh?|HH?|kk?|mm?|ss?|S{1,9}|x|X|zz?|ZZ?|.)";
+	std::string source = std::string(R"((\[[^\[]*\])|(\\)?([Hh]mm(ss)?|Mo|MM?M?M?|Do|DDDo|DD?D?D?|ddd?d?|do?|))")
+		+ std::string(R"(w[o|w]?|W[o|W]?|Qo?|YYYYYY|YYYYY|YYYY|YY|gg(ggg?)?|GG(GGG?)?)")
+		+ std::string(R"(|e|E|a|A|hh?|HH?|kk?|mm?|ss?|S{1,9}|x|X|zz?|ZZ?|.)");
 
 	std::regex regex(source, std::regex_constants::ECMAScript | std::regex_constants::extended);
 
@@ -14,7 +14,7 @@ static inline std::regex formattingTokens()
 static inline std::string removeFormattingTokens(std::string input) {
 	std::cmatch m;
 	std::stringstream stream;
-	if (std::regex_match(input, m, std::regex(R"(\[[\s\S])"))) {
+	if (std::regex_match(input, std::regex(R"(\[[\s\S])", std::regex_constants::ECMAScript))) {
 		return std::regex_replace(input, 
 			std::regex(R"(^\[|\]$)", std::regex_constants::ECMAScript | std::regex_constants::extended), "");
 	}
